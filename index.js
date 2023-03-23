32cc32a72b2dcd2e018ad13e98164f8a1bae0fd5
tpeterec <alex1996p3@gmail.com>
2023-03-23 09:10:36 -0800

    Finalized Exercise

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // index.js
-var http   = require('http');
-var routes = require('./routes');
+var http = require("http");
+var routes = require("./routes");
 
 var handleRequest = function (req, res) {
-  if(routes[req.url] !== undefined) {
+  if (routes[req.url] !== undefined) {
     routes[req.url](req, res);
   } else {
     res.end("404, no such route");
@@ -12,6 +12,6 @@ var handleRequest = function (req, res) {
 
 var server = http.createServer(handleRequest);
 
-server.listen(8000, function() {
+server.listen(8000, function () {
   console.log("Listening...");
 });
